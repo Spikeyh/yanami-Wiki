23b465918d9413501e0179dc5a568c46e0247b41
Spikeyh <1476743382@qq.com>
2024-09-01 17:01:58 +0800

    first commit

---
+++ src/Card/NewsCard.js
@@ -0,0 +1,21 @@
+import "./NewsCard.css";
+
+export default function NewsCard({ articleId, cover, title, publishDate, authorId,excerpt }) {
+    return (
+        <div href={`/article/${articleId}`} className="newsCard">
+            <img className="cover" src={cover} alt="cover" />
+            <div className="content">
+                <h3 className="title">{title}</h3>
+                <p style={{
+                    backgroundColor:"black",
+                    width:"100%",
+                    height:"2px",
+                    borderRadius:"2px",
+                }}/>
+                <p className="excerpt">{excerpt}</p>
+                <p className="author">By Author #{authorId}</p>
+                <p className="publishDate">{new Date(publishDate).toLocaleDateString()}</p>
+            </div>
+        </div>
+    );
+}
